4a7b3d56916edd06126423f2373caf8a5a1393ed
Scott Jones <scottj1s@gmail.com>
2025-11-11 09:38:00 -0500

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+﻿#include "pch.h"
+
+#include "winrt/Microsoft.Windows.ApplicationModel.WindowsAppRuntime.h"
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace Microsoft::Windows::ApplicationModel::WindowsAppRuntime;
+
+int main()
+{
+    init_apartment();
+    auto runtimeVersion = RuntimeInfo::AsString();
+    wprintf(L"Runtime Version: %s\n", runtimeVersion.c_str());
+}
